f3947de3bdfb648177972b9f1f9844cfde2fd03d
Sandi Hermawan <hrm.shandy05@gmail.com>
2025-06-21 05:57:23 +0700

    feat: add tailwindcss 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+
+  css: ['~/assets/css/main.css'],
 
   modules: [
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/test-utils'
-  ]
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
